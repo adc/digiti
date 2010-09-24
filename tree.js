927812118afe0a94131848f56fa8dd41e3dc6b40
Your Name <defend.the.world@gmail.com>
2010-09-23 20:04:09 -0400

    Bug fixes in tree.js, rerendering example in cnvas_html

--- tree.js
+++ tree.js
@@ -1,4 +1,4 @@
-operators = ['*', '/', '+', '-'];
+operators = ['=', '*', '/', '+', '-'];
 
 function node()
 {
@@ -12,39 +12,39 @@ function mathTree()
 {
 	this.root = new node();
 	this.buildTree = function(arr, place) { add(this.root, arr, place)};
-	this.printTree = function() {print_infix(this.root)};
+	this.printTree = function() { return print_infix(this.root)};
 }
 
 function print_infix(root)
 {
 	var arr = [];
-	if(root.left != "NULL")
-		arr.concat(print_infix(root.left))
+	if(root.left != null)
+		arr = arr.concat(print_infix(root.left))
 
-	arr.concat(root);
+	arr = arr.concat(root);
 
-	if(root.right != "NULL")
-		arr.concat(print(root.right));
+	if(root.right != null)
+		arr = arr.concat(print_infix(root.right));
 	
 	return arr;
 }
 
 function add(root, arr, curr)
-{
-	root.value = arr[curr];
+{  
+	root.value = arr[curr].value;
 
 	root.right = new node();
-	if(operators.indexOf(arr[curr-1]) != -1)
-		curr = add(root.right, curr-1);
+	if(operators.indexOf(arr[curr-1].value) != -1)
+		curr = add(root.right, arr, curr-1);
 	else
-		root.right.value = arr[--curr];
+		root.right.value = arr[--curr].value;
 	root.right.par = root;
 
 	root.left = new node();
-	if(operators.indexOf(arr[curr-1]) != -1)
-		curr = add(root.left, curr-1);
+	if(operators.indexOf(arr[curr-1].value) != -1)
+		curr = add(root.left, arr, curr-1);
 	else
-		root.left.value = arr[--curr];
+		root.left.value = arr[--curr].value;
 	root.left.par = root;
 
 	return curr;
